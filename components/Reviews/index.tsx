1eed91f79f1d80e0be8572136e7768f8f99fa7f2
Muees99 <132849693+Muees99@users.noreply.github.com>
2024-10-19 20:58:14 +0100

    Review Cards

--- components/Reviews/index.tsx
+++ components/Reviews/index.tsx
@@ -16,7 +16,7 @@ const reviewData: ReviewData[] = [
   {
     image: "/assets/Alice.svg",
     header: "Alice Smith",
-    subheader: "Dog Lover",
+    subheader: "CEO Eronaman",
     content:
       "On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure.",
     rating: 5,
@@ -24,7 +24,7 @@ const reviewData: ReviewData[] = [
   {
     image: "/assets/Bob.svg",
     header: "Bob Johnson",
-    subheader: "Animal Enthusiast",
+    subheader: "CEO Universal",
     content:
       "Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus.",
     rating: 4,
